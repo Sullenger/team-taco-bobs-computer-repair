da707a31515d6fd759cf42d822e36650a13d1afb
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-10-24 20:52:58 -0600

    adding user update and delete

--- server/routes/crud-api.js
+++ server/routes/crud-api.js
@@ -7,92 +7,131 @@
 ;===========================================
 */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const User = require('../models/user');
+const User = require("../models/user");
 
-  // Create user
+// Create user
 router.post("/users/registration", function(req, res, next) {
-    User.findOne({ username: req.body.username }, function(err, user) {
-      if (err) {
-        console.log(err);
-        return next(err);
-      } else {
-        if (!user) {
-  
-          // hash password first
-          bcrypt.hash(req.body.password, 10).then(hash => {
-  
-            let user = new User ({
-              username: req.body.username,
-              email: req.body.email,
-              password: hash,
-              name_first: req.body.name_first,
-              name_last: req.body.name_last,
-              phone_number: req.body.phone_number,
-              roles: req.body.roles,
-              address: req.body.address,
-              security_questions: req.body.security_questions,
-              invoices: req.body.invoices,
-              date_created: req.body.dateCreated,
-              date_updated: req.body.dateUpdated
-            });
-            
-            //save user to DB
-            user.save().then( result => {
-              res.status(201).json({
-                message: "user created!",
-                result: result
-              });
-            });
-          })
-          
-          // User.create(user, function(req, res, next) {
-          //   if (err) {
-          //     console.log(err);
-          //     return next(err);
-          //   } else {
-          //     console.log(newUser);
-          //     res.json(newUser);
-          //   }
-          // });
-  
-        } else {
-          console.log("Username Unavailable");
-          res.status(500).send({
-            text: "Username Unavailable",
-            time_stamp: new Date()
+  User.findOne({ username: req.body.username }, function(err, user) {
+    if (err) {
+      console.log(err);
+      return next(err);
+    } else {
+      if (!user) {
+        // hash password first
+        bcrypt.hash(req.body.password, 10).then(hash => {
+          let user = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash,
+            name_first: req.body.name_first,
+            name_last: req.body.name_last,
+            phone_number: req.body.phone_number,
+            roles: req.body.roles,
+            address: req.body.address,
+            security_questions: req.body.security_questions,
+            date_created: req.body.dateCreated,
+            date_updated: req.body.dateUpdated
           });
+
+          //save user to DB
+          user.save().then(result => {
+            res.status(201).json({
+              message: "user created!",
+              result: result
+            });
+          });
+        });
+
+        // User.create(user, function(req, res, next) {
+        //   if (err) {
+        //     console.log(err);
+        //     return next(err);
+        //   } else {
+        //     console.log(newUser);
+        //     res.json(newUser);
+        //   }
+        // });
+      } else {
+        console.log("Username Unavailable");
+        res.status(500).send({
+          text: "Username Unavailable",
+          time_stamp: new Date()
+        });
+      }
+    }
+  });
+});
+
+// Find all users
+router.get("/users", function(req, res, next) {
+  User.find({}, function(err, users) {
+    if (err) {
+      console.log(err);
+      return next(err);
+    } else {
+      console.log(users);
+      res.json(users);
+    }
+  });
+});
+
+// Find by User Id
+router.get("/users/:id", function(req, res, next) {
+  User.findOne({ _id: req.params.id }, function(err, user) {
+    if (err) {
+      console.log(err);
+      return next(err);
+    } else {
+      console.log(user);
+      res.json(user);
+    }
+  });
+});
+
+// Update user by id
+router.put("/users/:id", function(req, res, next) {
+  User.findOne({ _id: req.params.id }, function(err, user) {
+    if (err) {
+      console.log(err);
+      return next(err);
+    } else {
+      console.log(user);
+      user.set({
+        username: req.body.username,
+        email: req.body.email,
+        name_first: req.body.name_first,
+        name_last: req.body.name_last,
+        phone_number: req.body.phone_number,
+        address: req.body.address,
+        date_updated: new Date()
+      });
+      user.save(function(err, updatedUser) {
+        if (err) {
+          console.log(err);
+          return next(err);
+        } else {
+          console.log(updatedUser);
+          res.json(updatedUser);
         }
-      }
-    });
+      });
+    }
   });
-  
-  // Find all users
-  router.get("/users", function(req, res, next) {
-    User.find({}, function(err, users) {
-      if (err) {
-        console.log(err);
-        return next(err);
-      } else {
-        console.log(users);
-        res.json(users);
-      }
-    });
+});
+
+// delete user by id
+router.delete("/users/:id", function(req, res, next) {
+  User.findByIdAndDelete({ _id: req.params.id }, function(err, user) {
+    if (err) {
+      console.log(err);
+      return next(err);
+    } else {
+      console.log(user);
+      res.json(user)
+    }
   });
-  
-  // Find by User Id
-  router.get("/users/:id", function(req, res, next) {
-    User.findOne({ _id: req.params.id }, function(err, user) {
-      if (err) {
-        console.log(err);
-        return next(err);
-      } else {
-        console.log(user);
-        res.json(user);
-      }
-    });
-  });
+});
 
 module.exports = router;
