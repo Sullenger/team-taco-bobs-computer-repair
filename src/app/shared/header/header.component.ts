b486ff39ffb6a2acd9fe5eca1b14b92c2a1c1b54
Lea <leatrueworthy@Leas-iMac.local>
2019-10-26 14:37:23 -0700

    color

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private cookie: CookieService) { }
 
+  logout() {
+    console.log('You have logged out');
+    this.cookie.delete('isAuthenticated');
+    //this.router.navigate(["/login"]); will bring back when pages are created
+  }
   ngOnInit() {
   }
-
+  navHome() {
+    //this.router.navigate(["/home"]);
+  }
 }
