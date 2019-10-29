de288344cb77c9848d791be1a93fd1ae7cddbb9a
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-10-28 20:35:23 -0600

    find by id, update, and delete functions added for security questions

--- server/routes/question-crud-api.js
+++ server/routes/question-crud-api.js
@@ -10,15 +10,11 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
-const securityQuestion = require('../models/securityQuestion');
+const SecurityQuestion = require('../models/securityQuestion');
 
 // Create Security Question
 router.post("/question", function(req, res, next) {
-  const question = {
-    question: req.body.question
-  };
-
-  securityQuestion.create(question, function(err, questions) {
+  SecurityQuestion.create({ question: req.body.question }, function(err, questions) {
     if (err) {
       console.log(err);
       return next(err);
@@ -31,13 +27,61 @@ router.post("/question", function(req, res, next) {
 
 // Find all Security Question
 router.get("/question", function(req, res, next) {
-  securityQuestion.find({}, function(err, questions) {
+  SecurityQuestion.find({}, function(err, questions) {
     if (err) {
       console.log(err);
       return next(err);
     } else {
       console.log(questions);
       res.json(questions);
+    }
+  });
+});
+
+// Find Security Question by id
+router.get("/question/:id", function(req, res, next) {
+  SecurityQuestion.findOne({ _id: req.params.id }, function(err, question) {
+    if (err) {
+      console.log(err);
+      return next(err);
+    } else {
+      console.log(question);
+      res.json(question);
+    }
+  });
+});
+
+// Update security question by id
+router.put("/question/:id", function(req, res, next) {
+  SecurityQuestion.findOne({ _id: req.params.id }, function(err, question) {
+    if (err) {
+      console.log(err);
+      return next(err);
+    } else {
+      console.log(question);
+      question.set({question: req.body.question});
+      question.save(function(err, updatedSecurityQuestion) {
+        if (err) {
+          console.log(err);
+          return next(err);
+        } else {
+          console.log(updatedSecurityQuestion);
+          res.json(updatedSecurityQuestion);
+        }
+      });
+    }
+  });
+});
+
+// delete security question by id
+router.delete("/question/:id", function(req, res, next) {
+  SecurityQuestion.findByIdAndDelete({ _id: req.params.id }, function(err, question) {
+    if (err) {
+      console.log(err);
+      return next(err);
+    } else {
+      console.log(question);
+      res.json(question)
     }
   });
 });
