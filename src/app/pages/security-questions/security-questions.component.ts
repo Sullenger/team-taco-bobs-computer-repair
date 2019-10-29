67daa62992915781c425788a65abaf238005842a
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-10-28 21:00:05 -0600

    minor table changes

--- src/app/pages/security-questions/security-questions.component.ts
+++ src/app/pages/security-questions/security-questions.component.ts
@@ -1,33 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 // import { FormBuilder, Validators } from '@angular/forms';
 // import { CookieService } from 'ngx-cookie-service';
 // import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-security-questions',
-  templateUrl: './security-questions.component.html',
-  styleUrls: ['./security-questions.component.css']
+  selector: "app-security-questions",
+  templateUrl: "./security-questions.component.html",
+  styleUrls: ["./security-questions.component.css"]
 })
 export class SecurityQuestionsComponent implements OnInit {
   questions: any;
   errorMessage: string;
 
-  constructor(
-    private http: HttpClient,
-    // private fb: FormBuilder,
-    // private cookie: CookieService,
-    // private router: Router
-    ) { }
+  constructor(private http: HttpClient) // private fb: FormBuilder,
+  // private cookie: CookieService,
+  // private router: Router
+  {}
 
   ngOnInit() {
-    this.http.get('/questions/api/question').subscribe(res => {
+    this.http.get("/questions/api/question").subscribe(res => {
       if (res) {
-        return this.questions = res;
-        console.log(this.questions)
+        return (this.questions = res);
+        console.log(this.questions);
       } else {
-        return this.errorMessage = "No questions found";
+        return (this.errorMessage = "No questions found");
       }
-    })
+    });
   }
 }
