143f324e2026fbfe88d12afb74346ac6c9dc6c9b
ethan townsend <eetfitness@gmail.com>
2019-11-04 19:23:50 -0800

    adding error interceptor

---
+++ src/app/error-interceptor.ts
@@ -0,0 +1,37 @@
+/*
+============================================
+; Title: WEB	450	Bob’s	Computer	Repair	Shop
+; Author: Ethan Townsend, Lea Trueworthy, Natasha Whitmer, and Jason Sullenger
+; Date: 17 October 2019
+; Description: End-to-end billing system for Bob's Computer - MEAN stack
+;===========================================
+*/
+
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ErrorInterceptorComponent } from './pages/error-interceptor/error-interceptor.component';
+
+@Injectable()   
+
+export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(private dialog: MatDialog) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        return next.handle(req).pipe(
+            catchError((error: HttpErrorResponse) => {
+                // let errorMessage = 'An unknown error has occured..'
+                // if(error.error.message) {
+                //     errorMessage = error.error.message;
+                // }
+                // this.dialog.open(ErrorInterceptorComponent, {data: {message: errorMessage }});
+                // console.log(error)
+                alert(error.error.message)
+                return throwError(error);
+            })
+        )
+    }
+}
---
+++ src/app/pages/error-interceptor/error-interceptor.component.ts
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title: WEB	450	Bob’s	Computer	Repair	Shop
+; Author: Ethan Townsend, Lea Trueworthy, Natasha Whitmer, and Jason Sullenger
+; Date: 17 October 2019
+; Description: End-to-end billing system for Bob's Computer - MEAN stack
+;===========================================
+*/
+
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-error-handler',
+  templateUrl: './error-interceptor.component.html',
+  styleUrls: ['./error-interceptor.component.css']
+})
+export class ErrorInterceptorComponent implements OnInit {
+
+  message = 'A gosh darn error has done occured ayyyut';
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {message: string}) { }
+
+  ngOnInit() {
+  }
+
+}
