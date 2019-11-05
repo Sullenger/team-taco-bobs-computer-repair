b005954e211a409fc6f9999aeb5f2678114778f9
ethan townsend <eetfitness@gmail.com>
2019-11-04 19:36:29 -0800

    edit error interceptor

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -12,17 +12,23 @@ import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
 import { ErrorInterceptorComponent } from './pages/error-interceptor/error-interceptor.component';
 
 @Injectable()   
 
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(private dialog: MatDialog) {}
+    constructor(private dialog: MatDialog, private router: Router) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         return next.handle(req).pipe(
             catchError((error: HttpErrorResponse) => {
+
+                if([500].indexOf(error.status) !== -1){
+                    this.router.navigate(['/sesson/500'])
+                }
+
                 // let errorMessage = 'An unknown error has occured..'
                 // if(error.error.message) {
                 //     errorMessage = error.error.message;
